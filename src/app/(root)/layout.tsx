8c7d92a28888a0df879d6e3a56f1cdbe1b570020
ymohit1603 <mohityadav0330@gmail.com>
2024-09-24 14:35:00 +0530

    add: home page

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,13 +1,14 @@
-
+import  Sidebar  from "@/components/Sidebar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // const loggedIn
   return (
-      <main>
-          SIDEBAR
+      <main className="flex h-screen w-full font-inter">
+          <Sidebar/>
           {children}
     </main>
   );
