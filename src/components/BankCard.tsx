95242b087664603a74c43bb29234455e7caca548
ymohit1603 <mohityadav0330@gmail.com>
2024-10-19 18:41:44 +0530

    add: bank card for user

--- src/components/BankCard.tsx
+++ src/components/BankCard.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import Copy from './Copy'
-// import Copy from './Copy'
 
 const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) => {
 
